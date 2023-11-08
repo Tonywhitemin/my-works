6f143863ecdc814a0a66f6ff6b6576d88916a36a
Tonywhitemin <88414200+Tonywhitemin@users.noreply.github.com>
2023-11-08 15:22:28 +0800

    Update function_send_mail.py將密碼處理掉

--- Nocode_combine_AutoML_ver11/function_send_mail.py
+++ Nocode_combine_AutoML_ver11/function_send_mail.py
@@ -6,8 +6,8 @@ import smtplib
 def send_email(frommail, tomail, text):
     smtp_server = 'smtp.gmail.com'  # Gmail Hostname
     smtp_server_port = 587  # Gmail Server Port
-    smtp_server_account = 'tonywhitemin@gmail.com'  # Gmail Account Name
-    smtp_server_password = 'zquoaeownjuqhqwb'  #230608 Gmail 應用程式密碼，不是一般登入信箱的密碼喔
+    smtp_server_account = 123456789@gmail.com'  # Gmail Account Name
+    smtp_server_password = '3213t23y2y'  #230608 Gmail 應用程式密碼，不是一般登入信箱的密碼喔
     #應用程式密碼可參考：https://myaccount.google.com/apppasswords
     server = smtplib.SMTP(smtp_server, smtp_server_port)  # setting SMTP Server
     server.starttls()  # 信件加密
